ffe8ed3f367ad1f08b6f15e696c1a351176caffd
nairamangasaryan <mangasaryannaira@gmail.com>
2023-07-13 22:29:11 +0300

    Added STATIC_ROOT

--- portfolio_bootstrap/portfolio_bootstrap/settings.py
+++ portfolio_bootstrap/portfolio_bootstrap/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ef)^0sg+^^^6)!i1l$ygk=@p-&y)(yoyc5!90*1&p)5x6x#6^!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["nairamangasaryan.pythonanywhere.com"]
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 # Default primary key field type
